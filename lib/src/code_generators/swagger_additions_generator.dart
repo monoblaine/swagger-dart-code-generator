2abf52d39002e6736f2956bf8fe325f9197755bd
Serhan Apaydın <zerhan@gmail.com>
2024-06-19 02:58:10 +0300

    lint

--- lib/src/code_generators/swagger_additions_generator.dart
+++ lib/src/code_generators/swagger_additions_generator.dart
@@ -49,7 +49,7 @@ const List<Interceptor>? defaultInterceptors = [
     ${(options.authorizationHeaderValue.isEmpty ? '' : "'Authorization': '${options.authorizationHeaderValue}',")}
     'Accept-Language': 'tr,en-US;q=0.9,en;q=0.8',
     'X-Requested-With': 'XMLHttpRequest'
-  })
+  }),
 ];
 ''';
   }
